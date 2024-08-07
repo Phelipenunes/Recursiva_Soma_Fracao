eb37aef57037fc225d8b76719a3a5c650e700148
Phelipe <phelipesenac@gmail.com>
2024-08-07 17:26:09 -0300

    ,

--- src/controller/Metodos.java
+++ src/controller/Metodos.java
@@ -7,9 +7,9 @@ public class Metodos {
 	}
 	public double somafracao(double divisor){
 		if( divisor == 1){
-			return  1.0;
+			return  1.0;// caso o divisor seja 1, retornar 1, pois 1/1 é 1.
 		}else{
-			return 1/divisor + somafracao(divisor - 1);
+			return 1/divisor + somafracao(divisor - 1);// a função mantem o dividendo em 1 e subtrai 1 do divirsor somando com a fração anterior,
 		}
 	}
 }
